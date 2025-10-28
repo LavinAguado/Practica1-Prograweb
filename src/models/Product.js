cd6c3532589cfdf3819e2cc83987502da0bddfc1
Daniel Lavín Aguado <114480268+LavinAguado@users.noreply.github.com>
2025-10-28 18:59:06 +0100

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  image: String, // url o path si implementas subida
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
 
